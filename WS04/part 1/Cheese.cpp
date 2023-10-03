903bfce1553de9073f981b2252386a6a28519460
Danylo <danilovityk@gmail.com>
2023-10-03 17:22:33 -0400

    Part 1 ready to submit

---
+++ WS04/part 1/Cheese.cpp
@@ -0,0 +1,8 @@
+//
+//  Cheese.cpp
+//  WS4
+//
+//  Created by Danik on 03.10.2023.
+//
+
+#include "Cheese.hpp"
---
+++ WS04/part 1/Cheese.h
@@ -0,0 +1,5 @@
+#ifndef SDDS_CHEESE_H
+#define SDDS_CHEESE_H
+
+
+#endif /* Cheese_hpp */
