3ed0954ad2a220781ded675a48c053534acbc1c0
Danylo <danilovityk@gmail.com>
2023-10-10 21:17:06 -0400

    part 1 submission 1.0

---
+++ WS05/part 1/Book.cpp
@@ -0,0 +1,8 @@
+//
+//  Book.cpp
+//  WS5
+//
+//  Created by Danik on 10.10.2023.
+//
+
+#include "Book.hpp"
---
+++ WS05/part 1/Book.h
@@ -0,0 +1,13 @@
+//
+//  Book.hpp
+//  WS5
+//
+//  Created by Danik on 10.10.2023.
+//
+
+#ifndef Book_hpp
+#define Book_hpp
+
+#include <stdio.h>
+
+#endif /* Book_hpp */
