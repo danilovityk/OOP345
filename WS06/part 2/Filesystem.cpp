896f5b40cca103364f2a81c3b788903fac25376f
Danylo <danilovityk@gmail.com>
2023-11-02 17:22:09 -0400

    half way through

---
+++ WS06/part 2/Filesystem.cpp
@@ -0,0 +1,8 @@
+//
+//  Filesystem.cpp
+//  WS6
+//
+//  Created by Danik on 01.11.2023.
+//
+
+#include "Filesystem.hpp"
---
+++ WS06/part 2/Filesystem.h
@@ -0,0 +1,6 @@
+#ifndef SDDS_FILESYSTEM_H
+#define SDDS_FILESYSTEM_H
+
+#include <stdio.h>
+
+#endif /* Filesystem_hpp */
