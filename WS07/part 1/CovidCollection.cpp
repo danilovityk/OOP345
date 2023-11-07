6d8806dc01c7e9f5fa3fe22d970cc19cd26c77d6
Danylo <danilovityk@gmail.com>
2023-11-06 20:12:30 -0500

    submission test 1

--- WS07/part 1/CovidCollection.cpp
+++ WS07/part 1/CovidCollection.cpp
@@ -1,8 +1,60 @@
-//
-//  CovidCollection.cpp
-//  Ws7
-//
-//  Created by Danik on 06.11.2023.
-//
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include "CovidCollection.h"
 
-#include "CovidCollection.hpp"
+using namespace std;
+
+namespace sdds{
+
+CovidCollection::CovidCollection(const std::string filename) {
+    fstream file(filename);
+    if(file.is_open()){
+        Covid covid;
+        string buffer;
+        while (std::getline(file, buffer)){
+                       
+            covid.m_country = buffer.substr(0, 25);
+            covid.m_city =  buffer.substr(25, 25);
+            covid.m_variant = buffer.substr(50, 25);;
+            covid.m_year = stod(buffer.substr(75, 5));
+            covid.m_numOfCases = stoll(buffer.substr(80, 5));
+            covid.m_numOfDeaths = stoll(buffer.substr(85, 5));
+            
+            covid.m_country = covid.m_country.erase(covid.m_country.find_last_not_of(' ') + 1);
+            covid.m_city = covid.m_city.erase(covid.m_city.find_last_not_of(' ') + 1);
+            covid.m_variant = covid.m_variant.erase(covid.m_variant.find_last_not_of(' ') + 1);
+            
+            if (covid.m_year < 2000 || covid.m_year > 2100) {covid.m_year = -1;}
+            
+            m_collection.push_back(covid);
+            
+        }
+        
+        
+       
+    }else{
+        throw "bad file name";
+    }
+    
+    file.close();
+}
+
+void CovidCollection::display(std::ostream &out) const {
+    for_each(m_collection.begin(), m_collection.end(), [&](Covid item){out << item;});
+}
+
+std::ostream& operator<<(std::ostream& out, const Covid& theCovid){
+    
+    out << left << "| " << setw(21) << theCovid.m_country << " | " << setw(15) << theCovid.m_city << " | " << setw(20) << theCovid.m_variant << right << " | ";
+    if (theCovid.m_year == -1) {out << "      ";} else {out << setw(6) << theCovid.m_year;}
+    out << " | " << setw(4) << theCovid.m_numOfCases << " | " << setw(3) << theCovid.m_numOfDeaths << " |" << endl;
+    return out;
+}
+
+}
+
+
--- WS07/part 1/CovidCollection.h
+++ WS07/part 1/CovidCollection.h
@@ -3,10 +3,23 @@
 
 namespace sdds{
 
-class CovidCollection{
-    
-    
+struct Covid{
+    std::string m_country;
+    std::string m_city;
+    std::string m_variant;
+    int m_year;
+    size_t m_numOfCases;
+    size_t m_numOfDeaths;
 };
+
+class CovidCollection {
+    std::vector<Covid> m_collection;
+public:
+    CovidCollection(const std::string filename);
+    void display(std::ostream& out) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Covid& theCovid);
 
 }
 
