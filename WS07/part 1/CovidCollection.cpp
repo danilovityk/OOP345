a003fce362652601c41b9daebd517f2a794acbb5
Danylo <danilovityk@gmail.com>
2023-11-06 20:12:08 -0500

    part one submission test

---
+++ WS07/part 1/CovidCollection.cpp
@@ -0,0 +1,8 @@
+//
+//  CovidCollection.cpp
+//  Ws7
+//
+//  Created by Danik on 06.11.2023.
+//
+
+#include "CovidCollection.hpp"
---
+++ WS07/part 1/CovidCollection.h
@@ -0,0 +1,13 @@
+#ifndef SDDS_COVIDCOLLECTION_H
+#define SDDS_COVIDCOLLECTION_H
+
+namespace sdds{
+
+class CovidCollection{
+    
+    
+};
+
+}
+
+#endif /* CovidCollection_hpp */
