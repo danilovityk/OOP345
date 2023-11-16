124848faf04f575156efae998e6437416c481209
Danylo <danilovityk@gmail.com>
2023-11-16 12:54:19 -0500

    utilities function changed

--- WS08/part 1/Utilities.cpp
+++ WS08/part 1/Utilities.cpp
@@ -11,32 +11,33 @@
 using namespace std;
 
 namespace sdds {
-	DataBase<Profile> excludeRaw(const DataBase<Profile>& allProfiles, const DataBase<Profile>& bannedProfiles) {
-		DataBase<Profile> result;
-		// TODO: Add your code here to build a collection of Profiles.
-		//         The result should contain only profiles from `allProfiles`
-		//         which are not in `bannedProfiles` using Raw Pointers.
-        for(size_t i = 0; i < allProfiles.size(); ++i){
-            for(size_t b = 0; b < bannedProfiles.size(); ++b){
-                if(allProfiles[i].m_age != bannedProfiles[b].m_age && allProfiles[i].m_name.first_name != bannedProfiles[b].m_name.first_name && allProfiles[i].m_name.last_name != bannedProfiles[b].m_name.last_name){
-                    Profile* aboba = new Profile(allProfiles[i].m_name, allProfiles[i].m_address, allProfiles[i].m_age);
-                    try{
-                        aboba->validateAddress();
-                        result += *aboba;
-                    }catch(const char *){
-                       
-                        //cerr << "addres is not valid in exclude raw function";
-                    }
-                    delete aboba;
-                    b = bannedProfiles.size();
+DataBase<Profile> excludeRaw(const DataBase<Profile>& allProfiles, const DataBase<Profile>& bannedProfiles) {
+        DataBase<Profile> result;
+        // TODO: Add your code here to build a collection of Profiles.
+        //         The result should contain only profiles from `allProfiles`
+        //         which are not in `bannedProfiles` using Raw Pointers.
+        for(size_t i = 0; i < allProfiles.size(); ++i) {
+            bool isBanned = false;
+
+            for(size_t j = 0; j < bannedProfiles.size(); ++j) {
+                if(allProfiles[i].m_age == bannedProfiles[j].m_age &&
+                    allProfiles[i].m_name.first_name == bannedProfiles[j].m_name.first_name &&
+                    allProfiles[i].m_name.last_name == bannedProfiles[j].m_name.last_name) {
+                    isBanned = true;
+                    j = bannedProfiles.size();
                 }
             }
+            if(!isBanned) {
+                Profile* prof = new Profile(allProfiles[i].m_name, allProfiles[i].m_address, allProfiles[i].m_age);
+                try {
+                    prof->validateAddress();
+                    result += *prof;
+                } catch(...) {
+                    
+                }
+                delete prof;
+            }
         }
-
-
-
-
-
-		return result;
-	}
+        return result;
+    }
 }
